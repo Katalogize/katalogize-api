b475d3cd1d911d24f5f73d560cd4e99b54eaf19a
JoaoMoraesJr <joaojr1997@gmail.com>
2022-11-08 16:08:19 -0300

    Expand official catalogs

--- src/main/java/com/katalogizegroup/katalogize/config/MongoConfig.java
+++ src/main/java/com/katalogizegroup/katalogize/config/MongoConfig.java
@@ -155,6 +155,12 @@ public class MongoConfig {
                             new ItemFieldNumber(3, "", 2001),
                             new ItemFieldNumber(4, "", 89),
                             new ItemFieldImage(5, "", Arrays.asList(new UploadFile("official/Movies/Shrek/Shrek1.jpg", null), new UploadFile("official/Movies/Shrek/Shrek2.jpg", null), new UploadFile("official/Movies/Shrek/Shrek3.jpg", null))))),
+                    new CatalogItem(catalogs.get(1).getId(), templates.get(2).getId(), "Charlie and the Chocolate Factory", Arrays.asList(
+                            new ItemFieldString(1, "", "A young boy wins a tour through the most magnificent chocolate factory in the world, led by the world's most unusual candy maker. When Willy Wonka decides to let five children into his chocolate factory, he decides to release five golden tickets in five separate chocolate bars, causing complete mayhem."),
+                            new ItemFieldString(2, "", "Tim Burton"),
+                            new ItemFieldNumber(3, "", 2005),
+                            new ItemFieldNumber(4, "", 115),
+                            new ItemFieldImage(5, "", Arrays.asList(new UploadFile("official/Movies/ChocolateFactory/ChocolateFactory1.jpg", null), new UploadFile("official/Movies/ChocolateFactory/ChocolateFactory2.jpg", null), new UploadFile("official/Movies/ChocolateFactory/ChocolateFactory3.jpg", null))))),
 
                     //Series
                     new CatalogItem(catalogs.get(2).getId(), templates.get(3).getId(), "American Horror Story", Arrays.asList(
@@ -163,6 +169,54 @@ public class MongoConfig {
                             new ItemFieldNumber(3, "", 2011),
                             new ItemFieldNumber(4, "", 103),
                             new ItemFieldImage(5, "", Arrays.asList(new UploadFile("official/Series/AHS/AHS1.jpg", null), new UploadFile("official/Series/AHS/AHS2.jpg", null), new UploadFile("official/Series/AHS/AHS3.jpg", null))))),
+                    new CatalogItem(catalogs.get(2).getId(), templates.get(3).getId(), "Arcane", Arrays.asList(
+                            new ItemFieldString(1, "", "Based on the world behind League of Legends, Arcane dives into the delicate balance between the rich, utopian city of Piltover Crest icon.png Piltover and the seedy, oppressed underground of Zaun Crest icon.png Zaun. Known across Runeterra as the \"city of progress,\" many of the most brilliant minds call these cities home. But the creation of hextech, a way for any person to control magical energy, threatens that balance. The story follows the origins of two iconic League champions-and the power that will tear them apart."),
+                            new ItemFieldString(2, "", "Netflix"),
+                            new ItemFieldNumber(3, "", 2021),
+                            new ItemFieldNumber(4, "", 9),
+                            new ItemFieldImage(5, "", Arrays.asList(new UploadFile("official/Series/Arcane/Arcane1.jpg", null), new UploadFile("official/Series/Arcane/Arcane2.jpg", null), new UploadFile("official/Series/Arcane/Arcane3.jpg", null))))),
+                    new CatalogItem(catalogs.get(2).getId(), templates.get(3).getId(), "Black Mirror", Arrays.asList(
+                            new ItemFieldString(1, "", "Set in a world only minutes from our own, \"Black Mirror\", a UK and USA non-hosted anthology series; unveils how modern technologies can backfire and be used against their makers, every episode set in a slightly different reality with different characters combating different types of technologies."),
+                            new ItemFieldString(2, "", "Netflix"),
+                            new ItemFieldNumber(3, "", 2011),
+                            new ItemFieldNumber(4, "", 22),
+                            new ItemFieldImage(5, "", Arrays.asList(new UploadFile("official/Series/BlackMirror/BlackMirror1.jpg", null), new UploadFile("official/Series/BlackMirror/BlackMirror2.jpg", null), new UploadFile("official/Series/BlackMirror/BlackMirror3.jpg", null))))),
+                    new CatalogItem(catalogs.get(2).getId(), templates.get(3).getId(), "Friends", Arrays.asList(
+                            new ItemFieldString(1, "", "Friends is a 90's Comedy TV show, based in Manhattan, about 6 friends who go through just about every life experience imaginable together; love, marriage, divorce, children, heartbreaks, fights, new jobs and job losses and all sorts of drama."),
+                            new ItemFieldString(2, "", "NBC"),
+                            new ItemFieldNumber(3, "", 1994),
+                            new ItemFieldNumber(4, "", 236),
+                            new ItemFieldImage(5, "", Arrays.asList(new UploadFile("official/Series/Friends/Friends1.jpg", null), new UploadFile("official/Series/Friends/Friends2.jpg", null), new UploadFile("official/Series/Friends/Friends3.jpg", null))))),
+                    new CatalogItem(catalogs.get(2).getId(), templates.get(3).getId(), "House", Arrays.asList(
+                            new ItemFieldString(1, "", "The series follows the life of anti-social, pain killer addict, witty and arrogant medical doctor Gregory House (Hugh Laurie) with only half a muscle in his right leg. He and his team of medical doctors try to cure complex and rare diseases from very ill ordinary people in the United States of America."),
+                            new ItemFieldString(2, "", "FOX"),
+                            new ItemFieldNumber(3, "", 2004),
+                            new ItemFieldNumber(4, "", 177),
+                            new ItemFieldImage(5, "", Arrays.asList(new UploadFile("official/Series/House/House1.jpg", null), new UploadFile("official/Series/House/House2.jpg", null), new UploadFile("official/Series/House/House3.jpg", null))))),
+                    new CatalogItem(catalogs.get(2).getId(), templates.get(3).getId(), "Love, Death + Robots", Arrays.asList(
+                            new ItemFieldString(1, "", "This collection of animated short stories spans several genres, including science fiction, fantasy, horror and comedy. World-class animation creators bring captivating stories to life in the form of a unique and visceral viewing experience. The animated anthology series includes tales that explore alternate histories, life for robots in a post-apocalyptic city and a plot for world domination by super-intelligent yogurt."),
+                            new ItemFieldString(2, "", "Netflix"),
+                            new ItemFieldNumber(3, "", 2019),
+                            new ItemFieldNumber(4, "", 35),
+                            new ItemFieldImage(5, "", Arrays.asList(new UploadFile("official/Series/LoveDeathRobots/LoveDeathRobots1.jpeg", null), new UploadFile("official/Series/LoveDeathRobots/LoveDeathRobots2.jpg", null), new UploadFile("official/Series/LoveDeathRobots/LoveDeathRobots3.jpg", null))))),
+                    new CatalogItem(catalogs.get(2).getId(), templates.get(3).getId(), "Stranger Things", Arrays.asList(
+                            new ItemFieldString(1, "", "When Will Byers suddenly goes missing, the whole town of Hawkins, Indiana, turns upside down. Many people are on the search for Will, including his mother Joyce, his brother Jonathan, his friends Mike, Dustin, and Lucas, Police Chief Jim Hopper, and other notable people. But one thing leads to another, creating a supernatural trail. And things get even weirder when a little girl with a shaved head comes into the story."),
+                            new ItemFieldString(2, "", "Netflix"),
+                            new ItemFieldNumber(3, "", 2016),
+                            new ItemFieldNumber(4, "", 34),
+                            new ItemFieldImage(5, "", Arrays.asList(new UploadFile("official/Series/StrangerThings/StrangerThings1.jpg", null), new UploadFile("official/Series/StrangerThings/StrangerThings2.jpg", null), new UploadFile("official/Series/StrangerThings/StrangerThings3.jpg", null))))),
+                    new CatalogItem(catalogs.get(2).getId(), templates.get(3).getId(), "The Big Bang Theory", Arrays.asList(
+                            new ItemFieldString(1, "", "The Big Bang Theory was centered on physicists Sheldon Cooper and Leonard Hofstadter, whose geeky and introverted lives were changed when Penny, an attractive waitress and aspiring actress, moved into the apartment across from theirs. Penny quickly became a part of Sheldon and Leonard's social group, which included the equally geeky engineer Howard Wolowitz and astrophysicist Raj Koothrappali. The group later expanded to include Howard's wife Bernadette Rostenkowski-Wolowitz and Sheldon's wife Amy Farrah Fowler."),
+                            new ItemFieldString(2, "", "CBS"),
+                            new ItemFieldNumber(3, "", 2007),
+                            new ItemFieldNumber(4, "", 279),
+                            new ItemFieldImage(5, "", Arrays.asList(new UploadFile("official/Series/TBBT/TBBT1.jpg", null), new UploadFile("official/Series/TBBT/TBBT2.jpg", null), new UploadFile("official/Series/TBBT/TBBT3.jpg", null))))),
+                    new CatalogItem(catalogs.get(2).getId(), templates.get(3).getId(), "House of the Dragon", Arrays.asList(
+                            new ItemFieldString(1, "", "House of the Dragon is set about 200 years before the events of Game of Thrones, 172 years before the birth of Daenerys Targaryen, descendant of the eponymous royal house, and 100 years after the Seven Kingdoms are united by the Targaryen Conquest. Featuring an ensemble cast, the show portrays the beginning of the end of House Targaryen, the events leading up to and covering the Targaryen civil war of succession, known as the \"Dance of the Dragons\"."),
+                            new ItemFieldString(2, "", "HBO"),
+                            new ItemFieldNumber(3, "", 2022),
+                            new ItemFieldNumber(4, "", 10),
+                            new ItemFieldImage(5, "", Arrays.asList(new UploadFile("official/Series/HouseOfTheDragon/HouseOfTheDragon1.jpg", null), new UploadFile("official/Series/HouseOfTheDragon/HouseOfTheDragon2.jpg", null), new UploadFile("official/Series/HouseOfTheDragon/HouseOfTheDragon3.jpg", null))))),
 
                     //Books
                     new CatalogItem(catalogs.get(3).getId(), templates.get(4).getId(), "Dark Matter", Arrays.asList(
@@ -171,9 +225,49 @@ public class MongoConfig {
                             new ItemFieldString(3, "", "Blake Crouch"),
                             new ItemFieldString(4, "", "Crown Publishing Group"),
                             new ItemFieldNumber(5, "", 2016),
-                            new ItemFieldNumber(6, "", 342)))
-
-
+                            new ItemFieldNumber(6, "", 342))),
+                    new CatalogItem(catalogs.get(3).getId(), templates.get(4).getId(), "The Woman in the Window", Arrays.asList(
+                            new ItemFieldString(1, "", "Anna Fox lives alone, a recluse in her New York City home, unable to venture outside. She spends her day drinking wine (maybe too much), watching old movies, recalling happier times... and spying on her neighbors. Then the Russells move into the house across the way: a father, mother, their teenaged son. The perfect family. But when Anna, gazing out her window one night, sees something she shouldn’t, her world begins to crumble—and its shocking secrets are laid bare. What is real? What is imagined? Who is in danger? Who is in control? In this diabolically gripping thriller, no one (and nothing) is what it seems."),
+                            new ItemFieldString(2, "", "Thriller"),
+                            new ItemFieldString(3, "", "AJ Finn"),
+                            new ItemFieldString(4, "", "William Morrow"),
+                            new ItemFieldNumber(5, "", 2018),
+                            new ItemFieldNumber(6, "", 448))),
+                    new CatalogItem(catalogs.get(3).getId(), templates.get(4).getId(), "Fahrenheit 451", Arrays.asList(
+                            new ItemFieldString(1, "", "Guy Montag is a fireman. His job is to destroy the most illegal of commodities, the printed book, along with the houses in which they are hidden. Montag never questions the destruction and ruin his actions produce, returning each day to his bland life and wife, Mildred, who spends all day with her television “family.” But when he meets an eccentric young neighbor, Clarisse, who introduces him to a past where people didn’t live in fear and to a present where one sees the world through the ideas in books instead of the mindless chatter of television, Montag begins to question everything he has ever known."),
+                            new ItemFieldString(2, "", "Dystopian"),
+                            new ItemFieldString(3, "", "Ray Bradbury"),
+                            new ItemFieldString(4, "", "Ballantine Books"),
+                            new ItemFieldNumber(5, "", 1953),
+                            new ItemFieldNumber(6, "", 256))),
+                    new CatalogItem(catalogs.get(3).getId(), templates.get(4).getId(), "The Da Vinci Code", Arrays.asList(
+                            new ItemFieldString(1, "", "The Da Vinci Code follows symbologist Robert Langdon and cryptologist Sophie Neveu after a murder in the Louvre Museum in Paris causes them to become involved in a battle between the Priory of Sion and Opus Dei over the possibility of Jesus Christ and Mary Magdalene having had a child together."),
+                            new ItemFieldString(2, "", "Mystery, Detective fiction"),
+                            new ItemFieldString(3, "", "Dan Brown"),
+                            new ItemFieldString(4, "", "Doubleday"),
+                            new ItemFieldNumber(5, "", 2003),
+                            new ItemFieldNumber(6, "", 489))),
+                    new CatalogItem(catalogs.get(3).getId(), templates.get(4).getId(), "Dune", Arrays.asList(
+                            new ItemFieldString(1, "", "Dune tells the story of young Paul Atreides, whose family accepts the stewardship of the planet Arrakis. While the planet is an inhospitable and sparsely populated desert wasteland, it is the only source of melange, or \"spice\", a drug that extends life and enhances mental abilities."),
+                            new ItemFieldString(2, "", "Science fiction"),
+                            new ItemFieldString(3, "", "Frank Herbert"),
+                            new ItemFieldString(4, "", "Chilton Books"),
+                            new ItemFieldNumber(5, "", 1965),
+                            new ItemFieldNumber(6, "", 412))),
+                    new CatalogItem(catalogs.get(3).getId(), templates.get(4).getId(), "I, Robot", Arrays.asList(
+                            new ItemFieldString(1, "", "The book is a set of stories about the first robotic machines and the problems and pitfalls of living with and working alongside them. The book is the first in a series of several novels about robots; it is famous for its Three Laws of Robotics that govern machine behavior, and for its device, the positronic brain, which contains a robot’s conscious intelligence."),
+                            new ItemFieldString(2, "", "Science fiction"),
+                            new ItemFieldString(3, "", "Isaac Asimov"),
+                            new ItemFieldString(4, "", "Gnome Press"),
+                            new ItemFieldNumber(5, "", 1965),
+                            new ItemFieldNumber(6, "", 253))),
+                    new CatalogItem(catalogs.get(3).getId(), templates.get(4).getId(), "Ready Player One", Arrays.asList(
+                            new ItemFieldString(1, "", "In the year 2044, reality is an ugly place. The only time teenage Wade Watts really feels alive is when he's jacked into the virtual utopia known as the OASIS. Wade's devoted his life to studying the puzzles hidden within this world's digital confines—puzzles that are based on their creator's obsession with the pop culture of decades past and that promise massive power and fortune to whoever can unlock them. But when Wade stumbles upon the first clue, he finds himself beset by players willing to kill to take this ultimate prize. The race is on, and if Wade's going to survive, he'll have to win—and confront the real world he's always been so desperate to escape."),
+                            new ItemFieldString(2, "", "Science fiction"),
+                            new ItemFieldString(3, "", "Ernest Cline"),
+                            new ItemFieldString(4, "", "Crown Publishing Group"),
+                            new ItemFieldNumber(5, "", 2011),
+                            new ItemFieldNumber(6, "", 374)))
                 );
 
                 userRepository.insert(users);
